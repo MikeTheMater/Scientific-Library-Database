f3d53963d8af5445caac00d178401a3bec604590
MikeTheMater <101823213+MikeTheMater@users.noreply.github.com>
2023-01-03 15:59:30 +0200

    Add files via upload
    Added the option to sign up.
    That is a very bad way to do it but is all i can do with the time

--- log_in.py
+++ log_in.py
@@ -14,3 +14,28 @@ def sign_in(conn):
         return True
     else:
         return False
+
+def sign_up(conn):
+    while(True):
+        username=input("Type the username you want:")
+        if username=="back" or username=="Back": break
+        cursor = conn.cursor()
+        cursor.execute("""SELECT u.UserID
+                FROM  User as u
+                WHERE u.UserID=?""",(username,))
+        result=cursor.fetchall()
+        print(result)
+        if len(result)>0:
+            print("Username already exists try another one. Or type 'back' to go back")
+        else:
+            while(True):
+                password=input("Type the password you want to use:")
+
+                user_sql='''INSERT INTO User(UserID, Password)
+                    VALUES(?,?)'''
+
+                cursor.execute(user_sql,(username, password))
+                conn.commit()
+                break
+            print("Sign up successfull.")
+            break
--- start_program.py
+++ start_program.py
@@ -6,28 +6,35 @@ import sqlite3
 if __name__=="__main__":
     conn=sqlite3.connect('Scientific_Libary.db')
 
-    while (True):
-        logStatus=log_in.sign_in(conn)
+    while(True):
+        option=input("If you want to sign in press 'Sign in'\nIf you don't have an account and want to sign up press 'Sign up'\nElse press quit to exit program\n")
+        if (option=="Sign up"):
+            log_in.sign_up(conn)
+        elif (option=="Sign in"):
+            while (True):
+                logStatus=log_in.sign_in(conn)
 
-        if logStatus:
-            print("Login successfull.")
-            fun=input("Do you want to read articles and books or upload files?(Read/Upload)\n")
+                if logStatus:
+                    print("Login successfull.")
+                    fun=input("Do you want to read articles and books or upload files?(Read/Upload)\n")
 
-            
+                    print("Connected to SQLite")
+                    while(True):
+                        if fun=="Read" or fun=="read":
+                            library_db.makechoice(conn)
+                            break
+                        elif fun=="Upload" or fun=="upload":
+                            upload_file.uploadchoice(conn)
+                            break
+                        else:
+                            input("Wrong input, try again.\n")
 
-            print("Connected to SQLite")
-            while(True):
-                if fun=="Read" or fun=="read":
-                    library_db.makechoice(conn)
-                    break
-                elif fun=="Upload" or fun=="upload":
-                    upload_file.uploadchoice(conn)
+                    conn.close()
                     break
                 else:
-                    input("Wrong input, try again.\n")
-
-            conn.close()
-            break
+                    print("Login failed. Try again.")
+        elif(option=="quit"):
+            quit()
         else:
-            print("Login failed. Try again.")
+            print("Wrong input, try again.")
         
